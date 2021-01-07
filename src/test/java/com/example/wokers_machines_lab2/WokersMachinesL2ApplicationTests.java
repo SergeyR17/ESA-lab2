8af909bd9f7c7693b5a31bca8a6fb8637779beb1
SergeyR17 <73442476+SergeyR17@users.noreply.github.com>
2021-01-07 20:22:38 +0400

    Add files via upload

---
+++ src/test/java/com/example/wokers_machines_lab2/WokersMachinesL2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wokers_machines_lab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WokersMachinesL2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
